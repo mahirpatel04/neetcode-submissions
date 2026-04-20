12fbd7e66cbeb903efabb57d7e26ea1a02673fdd
Mahir Patel <126451249+mahirpatel04@users.noreply.github.com>
2026-04-19 18:26:09 -0700

    Add: car-fleet - submission-9

---
+++ Data Structures & Algorithms/car-fleet/submission-9.py
@@ -0,0 +1,23 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        reverse = sorted([(p, s) for p,s in zip(position, speed)], key=lambda x: x[0], reverse=True)
+
+        stack = []
+
+
+        # for each car 
+        for i, car in enumerate(reverse):
+            time = (target - car[0])/car[1]
+            # if stack is empty we can simply add the current car into the stack
+            if len(stack) == 0:
+                # time is distance needed to travel divided by speed
+                stack.append(time)
+
+            # if stack not empty we have to check with top of stack and only add it in if it reached AFTER (in more time)
+            elif time > stack[-1]:
+                    stack.append(time)
+    
+                
+
+
+        return len(stack)
