df40ab333ee9a50b0d556f29b8db598a3d424df6
Mahir Patel <126451249+mahirpatel04@users.noreply.github.com>
2026-04-21 12:43:39 -0700

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        counts = {}
+        for n in nums:
+            counts[n] = counts.get(n, 0) + 1
+            if counts[n] > len(nums) / 2:
+                return n
+
+        return sorted([(c, n) for n, c in counts.items()], key=lambda x: x[0], reverse=True)[0][1]
