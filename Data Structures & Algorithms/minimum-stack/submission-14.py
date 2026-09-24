170c8ee211005d34a79fb26243440465b888f8a1
Mahir Patel <126451249+mahirpatel04@users.noreply.github.com>
2026-09-23 17:28:59 -0700

    Add: minimum-stack - submission-14

---
+++ Data Structures & Algorithms/minimum-stack/submission-14.py
@@ -0,0 +1,32 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minstack = []
+        
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+        if not self.minstack:
+            self.minstack.append(val)
+
+        else:
+            self.minstack.append(min(val, self.minstack[-1]))
+        
+
+    def pop(self) -> None:
+        if self.stack:
+            self.stack.pop()
+            self.minstack.pop()
+        
+
+    def top(self) -> int:
+        if self.stack:
+            return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        if self.minstack:
+            return self.minstack[-1]
+        
