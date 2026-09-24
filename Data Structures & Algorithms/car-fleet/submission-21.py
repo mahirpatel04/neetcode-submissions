8f4b6306182c591b5636e7b190f2718bdadffdb9
Mahir Patel <126451249+mahirpatel04@users.noreply.github.com>
2026-09-24 15:59:48 -0700

    Add: car-fleet - submission-21

---
+++ Data Structures & Algorithms/car-fleet/submission-21.py
@@ -0,0 +1,19 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        cars = sorted([(p, s) for p, s in zip(position, speed)], key = lambda x: x[0], reverse=True)
+
+        fleetTimes = []
+        numFleets = 0
+        for p, s in cars:
+            carTime = (target - p) / s
+
+            if (not fleetTimes) or (carTime > fleetTimes[-1]):
+                fleetTimes.append(carTime)
+                numFleets += 1
+
+
+
+            
+
+        
+        return numFleets
